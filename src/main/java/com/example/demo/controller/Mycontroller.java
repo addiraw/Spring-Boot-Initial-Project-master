4b8782209d0ae40f52e7bfac102f6cbe50869bff
addiraw <adityakumar1998rawat@gmail.com>
2022-03-23 19:05:49 +0530

    Revert "swagger addwith 'Found 1 custom documentation plugin(s)`"
    This reverts commit 4add0091be58ae59d7a97e24263b4348e6a61a22.

--- src/main/java/com/example/demo/controller/Mycontroller.java
+++ src/main/java/com/example/demo/controller/Mycontroller.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Course;
@@ -19,10 +19,9 @@ public class Mycontroller {
 	@Autowired
 	private CourseService courseService;
 
-	@RequestMapping("/")
+	@GetMapping("/home")
 	public String home() {
-		System.out.print("Welcome to course applciation");
-		return "home";
+		return "Welcome to course applciation";
 
 	}
 
@@ -60,5 +59,3 @@ public class Mycontroller {
 		return course;
 	}
 }
-//@Controller
-
